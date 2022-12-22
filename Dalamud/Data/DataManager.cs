b9c193d13c1f65c9675b92798a83848519b05998
Loskh <1020612624@qq.com>
2022-12-22 18:36:59 +0800

    [Fix] Chinese's server in lumina

--- Dalamud/Data/DataManager.cs
+++ Dalamud/Data/DataManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.IO;
+using System.Reflection;
 using System.Threading;
 
 using Dalamud.Interface.Internal;
@@ -121,7 +122,7 @@ public sealed class DataManager : IDisposable, IServiceType
                 }
             });
             this.luminaResourceThread.Start();
-            ChangeWorldForCN();
+            this.ChangeWorldForCN();
         }
         catch (Exception ex)
         {
@@ -134,25 +135,87 @@ public sealed class DataManager : IDisposable, IServiceType
     /// </summary>
     private void ChangeWorldForCN()
     {
-        var dcSheet = this.GameData.Excel.GetSheet<WorldDCGroupType>()!;
-        foreach (var (server,name) in CNWorld.ServerList)
+        var chineseWorldDCGroups = new[] {
+                new
+                {
+                    Name = "陆行鸟",
+                    Id   = 101u,
+                    Worlds = new[]
+                    {
+                        new { Id = 1175u, Name = "晨曦王座" },
+                        new { Id = 1174u, Name = "沃仙曦染" },
+                        new { Id = 1173u, Name = "宇宙和音" },
+                        new { Id = 1167u, Name = "红玉海"   },
+                        new { Id = 1060u, Name = "萌芽池"   },
+                        new { Id = 1081u, Name = "神意之地" },
+                        new { Id = 1044u, Name = "幻影群岛" },
+                        new { Id = 1042u, Name = "拉诺西亚" },
+                    },
+                },
+                new
+                {
+                   Name = "莫古力",
+                   Id   = 102u,
+                   Worlds = new[]
+                   {
+                        new { Id = 1121u, Name = "拂晓之间" },
+                        new { Id = 1166u, Name = "龙巢神殿" },
+                        new { Id = 1113u, Name = "旅人栈桥" },
+                        new { Id = 1076u, Name = "白金幻象" },
+                        new { Id = 1176u, Name = "梦羽宝境" },
+                        new { Id = 1171u, Name = "神拳痕"   },
+                        new { Id = 1170u, Name = "潮风亭"   },
+                        new { Id = 1172u, Name = "白银乡"   },
+                   },
+                },
+                new
+                {
+                   Name = "猫小胖",
+                   Id   = 103u,
+                   Worlds = new[]
+                   {
+                        new { Id = 1179u, Name = "琥珀原"   },
+                        new { Id = 1178u, Name = "柔风海湾" },
+                        new { Id = 1177u, Name = "海猫茶屋" },
+                        new { Id = 1169u, Name = "延夏"    },
+                        new { Id = 1106u, Name = "静语庄园" },
+                        new { Id = 1045u, Name = "摩杜纳"   },
+                        new { Id = 1043u, Name = "紫水栈桥" },
+                   },
+                },
+                new
+                {
+                   Name = "豆豆柴",
+                   Id   = 201u,
+                   Worlds = new[]
+                   {
+                        new { Id = 1201u, Name = "红茶川"    },
+                        new { Id = 1186u, Name = "伊修加德"  },
+                        new { Id = 1180u, Name = "太阳海岸"  },
+                        new { Id = 1183u, Name = "银泪湖"    },
+                        new { Id = 1192u, Name = "水晶塔"    },
+                        new { Id = 1202u, Name = "萨雷安"    },
+                        new { Id = 1203u, Name = "加雷马"    },
+                        new { Id = 1200u, Name = "亚马乌罗提" },
+                   },
+                },
+            };
+        var dcExcel = this.GameData.Excel.GetSheet<WorldDCGroupType>();
+        var worldExcel = this.GameData.Excel.GetSheet<World>();
+        foreach (var dc in chineseWorldDCGroups)
         {
-            var dc = dcSheet.GetRow(server);
-            dc.Name = new SeString(name);
-            if (dc.RowId == 201)
+            var dcToReplaced = dcExcel.GetRow(dc.Id);
+            dcToReplaced.Name = new SeString(dc.Name);
+            dcToReplaced.Region = 5;
+
+            foreach (var world in dc.Worlds)
             {
-                dc.RowId = 104;
-                dc.Region = 2;
+                var worldToUpdated = worldExcel.GetRow(world.Id);
+                worldToUpdated.IsPublic = true;
+                worldToUpdated.DataCenter = new LazyRow<WorldDCGroupType>(this.GameData, dc.Id, Lumina.Data.Language.ChineseSimplified);
             }
         }
 
-        var worldSheet = this.GameData.Excel.GetSheet<World>()!;
-        foreach (var (world,server) in CNWorld.WorldList)
-        {
-            var w = worldSheet.GetRow(world);
-            w.IsPublic = true;
-            w.DataCenter = new LazyRow<WorldDCGroupType>(this.GameData, server, Lumina.Data.Language.ChineseSimplified);
-        }
     }
 
     /// <summary>
