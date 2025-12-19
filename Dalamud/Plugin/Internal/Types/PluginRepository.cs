d138102097d8dd72a2f744f359c0f28c8720d6c0
Tsubasahane <wozaiha@gmail.com>
2025-12-19 20:52:36 +0800

    Update MainRepoUrl

--- Dalamud/Plugin/Internal/Types/PluginRepository.cs
+++ Dalamud/Plugin/Internal/Types/PluginRepository.cs
@@ -27,7 +27,7 @@ internal class PluginRepository
     /// <summary>
     /// The URL of the official main repository.
     /// </summary>
-    public const string MainRepoUrl = ServerAddress.MainAddress + "/Plugin/PluginMaster?apiLevel=13";
+    public const string MainRepoUrl = ServerAddress.MainAddress + "/Plugin/PluginMaster";
 
     private const int HttpRequestTimeoutSeconds = 20;
 
