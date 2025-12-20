f167eba47abb6491ced5a95dbad081f72f3b8819
FatChocobo <maplerecall@live.com>
2025-12-20 13:51:53 +0800

    fix: fallback preview image to goat

--- Dalamud/Interface/Internal/Windows/PluginImageCache.cs
+++ Dalamud/Interface/Internal/Windows/PluginImageCache.cs
@@ -46,7 +46,7 @@ internal class PluginImageCache : IInternalDisposableService
     public const int PluginIconHeight = 512;
 
     private const string MainRepoImageUrl = "https://raw.githubusercontent.com/ottercorp/DalamudPlugins/api6/{0}/{1}/images/{2}";
-    private const string MainRepoDip17ImageUrl = "https://raw.githubusercontent.com/ottercorp/PluginDistD17/main/{0}/{1}/images/{2}";
+    private const string MainRepoDip17ImageUrl = "https://raw.githubusercontent.com/goatcorp/PluginDistD17/main/{0}/{1}/images/{2}";
 
     [ServiceManager.ServiceDependency]
     private readonly HappyHttpClient happyHttpClient = Service<HappyHttpClient>.Get();
